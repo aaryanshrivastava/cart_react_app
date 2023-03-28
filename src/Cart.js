0ba0b822c9037614f2e419d077edb438ccb2d8dc
Aaryan Shrivastava <aaryanshrivastava03@gmail.com>
2023-03-29 00:52:33 +0530

    nnn

--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,10 @@
 // Keys given to elements in a list(in React), are unique among the siblings only.
 // props are similar to arguments, which we pass to functions.
+// props in react cannot be changed in the Component
+// props are used to pass information from one component to another
+// anything can be passed as a prop in react including eventhandlers.
+// React props can be accessed as an object or destructured
+// Props cannot be directly uploaded
 
 import React from "react";
 import CartItem from "./CartItem";
