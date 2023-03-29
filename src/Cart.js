4c2bf6594c6cef8c894d0c3fcf3ba4fec3530f09
Aaryan Shrivastava <aaryanshrivastava03@gmail.com>
2023-03-29 21:56:19 +0530

    added all features & Wind up

--- src/Cart.js
+++ src/Cart.js
@@ -9,85 +9,23 @@
 import React from "react";
 import CartItem from "./CartItem";
 
-class Cart extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [
-        {
-          price: 99,
-          title: "Watch",
-          qty: 1,
-          img: "",
-          id: 1,
-        },
-        {
-          price: 9999,
-          title: "Mobile Phone",
-          qty: 10,
-          img: "",
-          id: 2,
-        },
-        {
-          price: 25000,
-          title: "Laptop",
-          qty: 4,
-          img: "",
-          id: 3,
-        },
-      ],
-    };
-    // this.testing();
-  }
-  handleIncreaseQuantity = (product) => {
-    console.log("incresing the quantity of :", product);
-    const { products } = this.state;
-    const index = products.indexOf(product);
-
-    products[index].qty += 1;
-    this.setState({
-      products: products,
-    });
-  };
-
-  handleDecreaseQuantity = (product) => {
-    console.log("incresing the quantity of :", product);
-    const { products } = this.state;
-    const index = products.indexOf(product);
-
-    if (products[index].qty === 0) {
-      return;
-    }
-    products[index].qty -= 1;
-    this.setState({
-      products: products,
-    });
-  };
-  handleDeleteQuantity = (id) => {
-    const { products } = this.state;
-    const items = products.filter((item) => item.id !== id); // an array which contains products.
-    this.setState({
-      products: items,
-    });
-  };
-  render() {
-    const { products } = this.state;
-    return (
-      <div className="cart">
-        {products.map((product) => {
-          return (
-            <CartItem
-              product={product}
-              key={product.id}
-              onIncreaseQuantity={this.handleIncreaseQuantity}
-              onDecreaseQuantity={this.handleDecreaseQuantity}
-              onDeleteQuantity={this.handleDeleteQuantity}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Cart = (props) => {
+  const { products } = props;
+  return (
+    <div className="cart">
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteQuantity={props.onDeleteQuantity}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -8,10 +8,63 @@
 //           when you call setState() inside render() method,setState() invokes render().It gets into an infinite loop.
 //           When setState is called multiple times it is batched together and re render happens only once.
 //           The callbacks for both setStates will be fired after re-render.
+// state-transfer :: data flows in only one direction in React i.e., between parent and child,
+//           so no data can flow between the siblings.
 
 import React from "react";
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteQuantity } =
+    props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} src={product.img} />
+      </div>
+      <div className="right-block">
+        <div style={{ fontSize: 25 }}>{title}</div>
+        {/*Direct from Constructor*/}
+
+        <div style={{ color: "#777" }}>{price} ₹</div>
+        {/* from object De-Structuring */}
+
+        <div style={{ color: "#777" }}>Qty :{qty}</div>
+        <div className="cart-item-actions">
+          {/* buttons */}
+          <img
+            alt="increase"
+            className="actions-icons"
+            src="https://cdn-icons-png.flaticon.com/512/1828/1828919.png"
+            onClick={() => onIncreaseQuantity(product)}
+          />
+
+          <img
+            alt="decrease"
+            className="actions-icons"
+            src="https://cdn-icons-png.flaticon.com/512/1828/1828899.png"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="actions-icons"
+            src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
+            onClick={() => onDeleteQuantity(product.id)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  image: {
+    height: 100,
+    width: 100,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+
   // testing() {
   //   const promise = new Promise((resolve, reject) => {
   //     //setState acts like a synchronous call, so thus we can we the up to date state.
@@ -25,89 +78,32 @@ class CartItem extends React.Component {
   //   });
   // }
 
-  increaseQuantity = () => {
-    // console.log("this", this.state);
-    //set_state form 1
-    // this.setState({
-    //   title: "Some New Title",
-    // });
+  // increaseQuantity = () => {
+  //   // console.log("this", this.state);
+  //   //set_state form 1
+  //   // this.setState({
+  //   //   title: "Some New Title",
+  //   // });
 
-    //set_state form 2 , if prevState state is required then use this, else use the above ones.
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-  };
+  //   //set_state form 2 , if prevState state is required then use this, else use the above ones.
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty + 1,
+  //     };
+  //   });
+  // };
 
-  decreaseQuantity = () => {
-    const { qty } = this.state;
-    if (qty === 0) {
-      return;
-    }
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty - 1,
-      };
-    });
-  };
-  render() {
-    console.log("this.props", this.props);
-    const { price, title, qty } = this.props.product;
-    const {
-      product,
-      onIncreaseQuantity,
-      onDecreaseQuantity,
-      onDeleteQuantity,
-    } = this.props;
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img style={styles.image} />
-        </div>
-        <div className="right-block">
-          <div style={{ fontSize: 25 }}>{title}</div>
-          {/*Direct from Constructor*/}
-
-          <div style={{ color: "#777" }}>{price} ₹</div>
-          {/* from object De-Structuring */}
-
-          <div style={{ color: "#777" }}>Qty :{qty}</div>
-          <div className="cart-item-actions">
-            {/* buttons */}
-            <img
-              alt="increase"
-              className="actions-icons"
-              src="https://cdn-icons-png.flaticon.com/512/1828/1828919.png"
-              onClick={() => onIncreaseQuantity(product)}
-            />
-
-            <img
-              alt="decrease"
-              className="actions-icons"
-              src="https://cdn-icons-png.flaticon.com/512/1828/1828899.png"
-              onClick={() => onDecreaseQuantity(product)}
-            />
-            <img
-              alt="delete"
-              className="actions-icons"
-              src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
-              onClick={() => onDeleteQuantity(product.id)}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-const styles = {
-  image: {
-    height: 100,
-    width: 100,
-    borderRadius: 4,
-    background: "#ccc",
-  },
+  // decreaseQuantity = () => {
+  //   const { qty } = this.state;
+  //   if (qty === 0) {
+  //     return;
+  //   }
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty - 1,
+  //     };
+  //   });
+  // };
 };
 
 export default CartItem;
